4829763e6cbb3cb64968e3b00b2a5277fe5631b2
alekseylee <aleksey.lee@gmail.com>
2022-10-17 14:34:49 +0200

    init commit

---
+++ src/be/intecbrussel/vendingmachine/Item.java
@@ -0,0 +1,51 @@
+package be.intecbrussel.vendingmachine;
+
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item (String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public Item(Item source) {
+        this.name = source.name;
+        this.price = source.price;
+        this.quantity = source.quantity;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return this.name + ": " + this.price + " ("+this.quantity+")";
+    }
+
+
+
+}
